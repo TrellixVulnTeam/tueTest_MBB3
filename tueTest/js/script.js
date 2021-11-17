6a472f0a9b11b916bca1685236eba509d2873282
Grishchenko Igor <Sherkiosp@gmail.com>
2021-11-17 22:33:02 +0500

    Кнопка добавления темы

--- tueTest/js/script.js
+++ tueTest/js/script.js
@@ -21,61 +21,7 @@ document.querySelector('body').classList.add('no-webp');
 //      // перезаписываем страницу при загрузке
 // });
 
-// Добавляем кнопки для функций после нажатия на "Редактировать"
-function addChanges() {
-	
-	const invisible = document.querySelectorAll('.invisible');
-	const buttonVisible = document.querySelectorAll('.mustVisible');
-	const buttonInvisible = document.querySelectorAll('.mustInvisible');
-	let nameChapter = document.querySelector('#name').textContent;
-	let inputName = document.querySelector('.discipline__changeName');
-	// Добавляем кнопки для редактирования
-	invisible.forEach(invisible => {invisible.classList.add('visible')});
-	invisible.forEach(invisible => {invisible.classList.remove('invisible')});
-	
-	// Убираем кнопку Добавления и Редактирования
-	buttonVisible.forEach(buttonVisible => {buttonVisible.classList.add('mustInvisible')});
-	buttonVisible.forEach(buttonVisible => {buttonVisible.classList.remove('mustVisible')});
-	
-	// Добавляем кнопку для сохранения
-	buttonInvisible.forEach(buttonInvisible => {buttonInvisible.classList.add('mustVisible')});
-	buttonInvisible.forEach(buttonInvisible => {buttonInvisible.classList.remove('mustInvisible')});
 
-	// Добавление имени в строку изменения
-	inputName.value = nameChapter;
-
-	// Сохраняем DOM для перезагрузок
-    //sessionStorage.setItem("page",document.getElementsByTagName("html")[0].innerHTML);
-
-    
-    
-
-}
-// Сохраняем изменения
-function saveChanges() {
-	
-	const visible = document.querySelectorAll('.visible');
-	const buttonVisible = document.querySelectorAll('.mustVisible');
-	const buttonInvisible = document.querySelectorAll('.mustInvisible');
-	let inputName = document.getElementById('changeName').value;
-	let nameChapter = document.getElementById('name');
-	// Скрываем кнопки редактирования
-	visible.forEach(visible => {visible.classList.add('invisible')})
-	visible.forEach(visible => {visible.classList.remove('visible')})
-
-	// Убираем кнопку сохранения
-	buttonVisible.forEach(buttonVisible => {buttonVisible.classList.add('mustInvisible')});
-	buttonVisible.forEach(buttonVisible => {buttonVisible.classList.remove('mustVisible')});
-	
-	// Добавляем кнопку Добавления и Редактирования
-	buttonInvisible.forEach(buttonInvisible => {buttonInvisible.classList.add('mustVisible')});
-	buttonInvisible.forEach(buttonInvisible => {buttonInvisible.classList.remove('mustInvisible')});
-
-
-	nameChapter.innerHTML = inputName;
-	console.log(inputName);
-
-}
 //Сворачивание и разворачивание темы
 $(document).on("click", ".discipline__name", function(e){
   var accord = $(event.target).parent().children(".invisibleTheme");
@@ -86,7 +32,7 @@ $(document).on("click", ".discipline__name", function(e){
   }
   
 });
-
+// убирает у input disabled
 $(document).on("click", ".discipline__change", function(e){
   var changeB = $(event.target);
   var input = changeB.parent().parent().children("input");
