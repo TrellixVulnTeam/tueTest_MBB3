832134ae6bd24b24a504f336015a889c32201506
Grishchenko Igor <Sherkiosp@gmail.com>
2021-11-19 21:37:23 +0500

    Добавил добавления новых тем и изменил отображение изменения изменить, сохранить и добавил удаление тем

--- tueTest/js/script.js
+++ tueTest/js/script.js
@@ -25,10 +25,14 @@ document.querySelector('body').classList.add('no-webp');
 //Сворачивание и разворачивание темы
 $(document).on("click", ".discipline__name", function(e){
   var accord = $(event.target).parent().children(".invisibleTheme");
-  if ((accord.css('display') == 'none') && (accord.hasClass("mustVisible"))) {
+  var buttonAdd =accord.parent().children(".discipline__themeAdd");
+  console.log(accord.attr('class'));
+  if ((accord.css('display') == 'none') && (accord.hasClass("mustVisible")) && (buttonAdd.css('display') == 'none') && (buttonAdd.hasClass("mustVisible"))) {
   	accord.css({'display':'grid'});
+  	buttonAdd.css({'display':'block'});
   } else {
   	accord.css({'display':'none'});
+  	buttonAdd.css({'display':'none'});
   }
   
 });
@@ -37,14 +41,44 @@ $(document).on("click", ".discipline__change", function(e){
   var changeB = $(event.target);
   var input = changeB.parent().parent().children("input");
   input.removeAttr('disabled');
+  changeB.removeClass("discipline__change")
   changeB.addClass("discipline__save")
-  console.log(input);
+  changeB.text('Сохр');
 });
-
 $(document).on("click", ".discipline__save", function(e){
   var changeB = $(event.target);
   var input = changeB.parent().parent().children("input");
   input.attr('disabled', 'disabled');
+  changeB.addClass("discipline__change")
   changeB.removeClass("discipline__save")
-  console.log(input);
+  changeB.text('Изм');
 });
+
+// Добавление новой темы
+$(document).on("click", ".discipline__themeAdd", function(e){
+  var button = $(event.target);
+  newTheme = $('<div class="discipline__chapter__theme invisibleTheme mustVisible">' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__name"></input>' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__lectures"></input>' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__practise"></input>' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__laboratory"></input>'  +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__ksr"></input>' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__isw"></input>' +
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__allHours"></input>' + 
+                	'<input type="text"  placeholder="" name=""class="discipline__theme__brs"></input>' +
+                	'<div class="discipline__theme__buttonsOfChanges discipline__buttonsOfChanges">' +
+						'<a href="#" class="discipline__save">Сохранить</a>' +
+						'<a href="#" class="discipline__delet">удал</a>' +
+					'</div>' +
+                '</div>');
+  button.before(newTheme);
+  console.log(button.attr('class'));
+});
+
+//Удаление темы
+$(document).on("click", ".discipline__delet", function(e){
+  var button = $(event.target);
+  button.parent().parent().remove();
+  console.log(button.parent().parent().attr('class'));
+});
+
